d63026378e1947c44a7774ef1a8967fef6d6aa43
Stuart Hallows <stuart.hallows@gmail.com>
2024-04-19 20:17:30 +0930

    wip

--- Jwt.Auth/Authorization/PermissionAuthorizationPolicyProvider.cs
+++ Jwt.Auth/Authorization/PermissionAuthorizationPolicyProvider.cs
@@ -3,16 +3,20 @@ using Microsoft.Extensions.Options;
 
 namespace Jwt.Auth.Authorization;
 
+// https://youtu.be/SZtZuvcMBA0?list=PLYpjLpq5ZDGtJOHUbv7KHuxtYLk1nJPw5&t=713
+
+/// <summary>
+/// Rather than explicitly create a new policy for each Permission requirements are created on demand.
+/// </summary>
 public class PermissionAuthorizationPolicyProvider : DefaultAuthorizationPolicyProvider
 {
     public PermissionAuthorizationPolicyProvider(IOptions<AuthorizationOptions> options) : base(options)
     {
     }
     
-    // https://youtu.be/SZtZuvcMBA0?list=PLYpjLpq5ZDGtJOHUbv7KHuxtYLk1nJPw5&t=787
-    
     public override async Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
     {
+        // Check if the policy is already defined.
         AuthorizationPolicy? policy = await base.GetPolicyAsync(policyName);
 
         if (policy is not null)
@@ -20,6 +24,7 @@ public class PermissionAuthorizationPolicyProvider : DefaultAuthorizationPolicyP
             return policy;
         }
 
+        // If the policy is not defined, create a new one.
         return new AuthorizationPolicyBuilder()
             .AddRequirements(new PermissionRequirement(policyName))
             .Build();
